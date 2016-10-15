9825bd70d4c5db2d9396c6ae0a62f88e7897eca3
Tom Ross <tom@umpox.com>
2016-10-16 00:04:46 +0100

    Update controls

--- controls.js
+++ controls.js
@@ -1,13 +1,17 @@
-var box = document.getElementsByTagName('a-box');
+$(document).ready(function(){
+    var box = document.getElementsByTagName('a-box');
 
-var changeColor = function() {
-  this.setAttribute('color', 'green');
-};
-var returnColor = function() {
-  this.setAttribute('color', '#4CC3D9');  
-};
+    var changeColor = function() {
+        console.log('change');
+        this.setAttribute('color', 'green');
+    };
+    var returnColor = function() {
+        console.log('return');
+        this.setAttribute('color', '#4CC3D9');  
+    };
 
-for (var i = 0; i < box.length; i++) {
-    box[i].addEventListener('mouseenter', changeColor, false);
-    box[i].addEventListener('mouseleave', returnColor, false);
-};
+    for (var i = 0; i < box.length; i++) {
+        box[i].addEventListener('mouseenter', changeColor, false);
+        box[i].addEventListener('mouseleave', returnColor, false);
+    }
+});
