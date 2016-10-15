05269866934736151e19f2b3dfc035ce2358a989
Tom Ross <tom@umpox.com>
2016-10-15 22:15:42 +0100

    Add mouseenter/mouseleave logic

--- controls.js
+++ controls.js
@@ -1,10 +1,13 @@
 var box = document.getElementsByTagName('a-box');
 
+var changeColor = function() {
+  this.setAttribute('color', 'green');
+};
+var returnColor = function() {
+  this.setAttribute('color', '#4CC3D9');  
+};
 
 for (var i = 0; i < box.length; i++) {
-    box[i].addEventListener('mouseenter', myFunction(box[i]), false);
+    box[i].addEventListener('mouseenter', changeColor, false);
+    box[i].addEventListener('mouseleave', returnColor, false);
 }
-
-var myFunction = function(box) {
-    box.setAttribute('color', 'green');
-};
