d2226763223bd32208b3dfd6eaac9fb9319ffd5b
Tom Ross <tom@umpox.com>
2016-10-16 00:22:19 +0100

    Improve code efficiency using hover method

--- controls.js
+++ controls.js
@@ -1,18 +1,8 @@
 $(document).ready(function(){
-    var box = document.getElementsByTagName('a-box');
-
     var changeColor = function() {
         console.log('change');
         this.setAttribute('color', 'green');
     };
-    var returnColor = function() {
-        console.log('return');
-        this.setAttribute('color', '#4CC3D9');  
-    };
 
-    for (var i = 0; i < box.length; i++) {
-        box[i].mouseover(changeColor);
-        //box[i].addEventListener('mouseenter', changeColor, false);
-        //box[i].addEventListener('mouseleave', returnColor, false);
-    }
+    $('a-box').hover(changeColor);
 });
