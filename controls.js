e4825c78d80f3e1f9e8546e1d63be5931ae9eff6
Tom Ross <tom@umpox.com>
2016-10-16 00:25:50 +0100

    Remove unused console.logs

--- controls.js
+++ controls.js
@@ -2,11 +2,9 @@ $(document).ready(function(){
     var box = document.getElementsByTagName('a-box');
 
     var changeColor = function() {
-        console.log('change');
         this.setAttribute('color', 'green');
     };
     var returnColor = function() {
-        console.log('return');
         this.setAttribute('color', '#4CC3D9');  
     };
 
