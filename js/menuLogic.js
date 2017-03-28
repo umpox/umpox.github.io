feee50168dd5104a31f32b548202b6df8b48cf59
Tom Ross <tom@umpox.com>
2017-03-28 17:51:18 +0100

    Add google login auth

--- js/menuLogic.js
+++ js/menuLogic.js
@@ -52,6 +52,23 @@ var loginUser = function() {
     });
 };
 
+var loginUserGoogle = function() {
+    // Using a redirect.
+    firebase.auth().getRedirectResult().then(function(result) {
+        if (result.credential) {
+            // This gives you a Google Access Token.
+            var token = result.credential.accessToken;
+        }
+        var user = result.user;
+    });
+
+    // Start a sign in process for an unauthenticated user.
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('profile');
+    provider.addScope('email');
+    firebase.auth().signInWithRedirect(provider);
+};
+
 var registerUser = function() {
     var registerEmail = document.getElementById('registerEmail');
     var registerPassword = document.getElementById('registerPassword');
