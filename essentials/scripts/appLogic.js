4bfc91e84721dfce24cba9347671928e90980f68
Tom Ross <tom@umpox.com>
2017-04-15 23:08:48 +0100

    Migrate changes to server and minify code

--- essentials/scripts/appLogic.js
+++ essentials/scripts/appLogic.js
@@ -1,124 +1,23 @@
-//http://stackoverflow.com/questions/436411/where-should-i-put-script-tags-in-html-markup-->
-var newsContentArea;
-var loadingIcon;
-var refreshIcon;
-var menuButtons;
-
-document.addEventListener("DOMContentLoaded", function() { 
-    newsContentArea = document.getElementById('newsContentArea');
-    loadingIcon = document.getElementById('loading');
-    refreshIcon = document.getElementById('menuBanner-content-refresh');
-    menuButtons = document.getElementById('menuButtonArea');
-});
-
-var loadNewsData = function() {
-    menuButtons.classList.add('hidden');
-    newsContentArea.classList.remove('hidden');
-    //URL to load news data
-    var apiURL = 'https://content.guardianapis.com/world?show-fields=trailText&api-key=ac6c039d-e505-4c5e-ad69-0f7b1a29f98c';
-
-    //Variables to store specific news data
-    var newsTitle = '';
-    var newsDesc = '';
-    var newsURL = '';
-
-    //Make attempt to load URL data
-    var newsAPI = new apiCall();
-    newsAPI.get(apiURL, function(newsData) {
-        //Successfully loaded
-        //Hide loading icon
-        loadingIcon.classList.add('hidden');
-
-        //Convert string to JSON
-        newsData = JSON.parse(newsData);
-        var news = newsData.response.results;
-
-        //Only read the articles part of the JSON
-        for (var article in news) {
-            newsTitle = news[article].webTitle;
-            newsDesc = news[article].fields.trailText;
-            newsURL = news[article].webUrl;
-
-            newsURL = newsURL.replace('https://www', 'https://amp');
-
-            newsContentArea.innerHTML += `
-            <a class="pure-u-24-24 pure-button newsItem" rel="amphtml" href="${newsURL}">
+var newsContentArea;var weatherContentArea;var articleContentArea;var loadingIcon;var refreshIcon;var menuButtons;var backBtnContainer;var refreshBtnContainer;document.addEventListener("DOMContentLoaded",function(){loadingIcon=document.getElementById('loading');loadingIcon.classList.add('hidden');backBtnContainer=document.getElementById('menuBanner-content-back');refreshBtnContainer=document.getElementById('menuBanner-content-refresh');backBtnContainer.innerHTML='<img src="resources/back.svg">';refreshBtnContainer.innerHTML='<img src="resources/refresh.svg">';newsContentArea=document.getElementById('newsContentArea');weatherContentArea=document.getElementById('weatherContentArea');articleContentArea=document.getElementById('articleContentArea');refreshIcon=document.getElementById('menuBanner-content-refresh');menuButtons=document.getElementById('menuButtonArea')});var loadNewsData=function(){loadingIcon.classList.remove('hidden');menuButtons.classList.add('hidden');newsContentArea.classList.remove('hidden');backBtnContainer.classList.remove('hidden');refreshBtnContainer.classList.remove('hidden');var apiURL='https://content.guardianapis.com/world?show-fields=trailText&api-key=ac6c039d-e505-4c5e-ad69-0f7b1a29f98c';var currentTime=new Date();var newsTitle='';var newsDesc='';var newsURL='';var newsID;var newsAPI=new apiCall();newsAPI.get(apiURL,function(newsData){newsData=JSON.parse(newsData);var news=newsData.response.results;loadingIcon.classList.add('hidden');newsContentArea.innerHTML+=`
+            <a class="pure-u-24-24 newsMain" rel="amphtml">
+                <h4 class="newsItem-title">Last updated: ${currentTime.toLocaleTimeString()}</h4>                
+            </a>`;for(var article in news){newsTitle=news[article].webTitle;newsDesc=news[article].fields.trailText;newsURL=news[article].webUrl;newsID=news[article].id;newsURL=newsURL.replace('https://www','https://amp');newsContentArea.innerHTML+=`
+            <a class="pure-u-24-24 pure-button newsItem" onclick="loadArticle( '${newsID}' )" rel="amphtml">
                 <p class="newsItem-title">${newsTitle}</p>
                 <p class="newsItem-desc">${newsDesc}</p>
-            </a>`;
-        }
-    });
-};
-
-var loadWeatherData = function() {
-    menuButtons.classList.add('hidden');
-    weatherContentArea.classList.remove('hidden');
-
-    //URL to load weather data
-    var apiURL = 'https://api.darksky.net/forecast/fd7861deb521e1b6a124ba8b2c9ff871/53.4084,2.9916?exclude=currently,minutely,hourly,alerts,flags&units=si';
-
-    //Variables to store weather data
-    var weatherDate;
-    var weatherDesc;
-    var weatherTemp;
-
-    //Make attempt to load URL data
-    var weatherAPI = new apiCall();
-    weatherAPI.get(apiURL, function(weatherData) {
-        //Successfully loaded
-        //Hide loading icon
-        loadingIcon.classList.add('hidden');
-
-        //Convert string to JSON
-        weatherData = JSON.parse(weatherData);
-        var weather = weatherData.daily.data;
-
-        //Only read the articles part of the JSON
-        for (var day in weather) {
-            weatherDate = weather[day].time;
-            weatherDesc = weather[day].summary;
-            weatherTemp = weather[day].temperatureMax;
-
-            weatherContentArea.innerHTML += `
-            <a class="pure-u-24-24 pure-button weatherItem" rel="amphtml">
-                <p class="newsItem-title">${weatherTemp}</p>
-                <p class="newsItem-desc">${weatherDesc}</p>
-            </a>`;
-        }
-    });
-
-};
-
-var apiCall = function() {
-    this.get = function(apiURL, apiCallbackFunc) {
-        var apiRequest = new XMLHttpRequest();
-
-        apiRequest.onreadystatechange = function() { 
-            if (apiRequest.readyState == 4 && apiRequest.status == 200) {
-                apiCallbackFunc(apiRequest.responseText);
-                refreshIcon.classList.remove('refresh__rotate');
-            }
-        };
-
-        apiRequest.open( "GET", apiURL, true );            
-        apiRequest.send( null );
-    };
-};
-
-var refreshPage = function() {
-    refreshIcon.classList.add('refresh__rotate');
-    newsContentArea.innerHTML = "";
-    loadingIcon.classList.remove('hidden');
-    loadNewsData();
-};
-
-var returnToMenu = function() {
-    newsContentArea.classList.add('hidden');
-    weatherContentArea.classList.add('hidden');
-    menuButtons.classList.remove('hidden');
-};
-
-
-
-/* USED TO CALCULATE USERS LOCATION*/
-//http://maps.googleapis.com/maps/api/geocode/json?latlng=44.4647452,7.3553838
+            </a>`}})};var loadArticle=function(newsID){loadingIcon.classList.remove('hidden');newsContentArea.classList.add('hidden');articleContentArea.classList.remove('hidden');var apiURL=`https://content.guardianapis.com/${newsID}?show-blocks=body&api-key=ac6c039d-e505-4c5e-ad69-0f7b1a29f98c`;var articleTitle;var articleBody;var articleAPI=new apiCall();articleAPI.get(apiURL,function(articleData){articleData=JSON.parse(articleData);articleTitle=articleData.response.content.webTitle;articleBody=articleData.response.content.blocks.body[0].bodyTextSummary;loadingIcon.classList.add('hidden');articleContentArea.innerHTML=`
+        <h2 id="articleTitle">${articleTitle}</h2>
+        <p id="articleBody">${articleBody}</p>`})};var loadWeatherData=function(){loadingIcon.classList.remove('hidden');menuButtons.classList.add('hidden');weatherContentArea.classList.remove('hidden');backBtnContainer.classList.remove('hidden');refreshBtnContainer.classList.remove('hidden');var apiURL='http://api.openweathermap.org/data/2.5/forecast/daily?appid=4e65e289b57c474e618e2e41e73484f3&units=metric&q=Liverpool';var currentTime=new Date();var weatherDate;var weatherDesc;var weatherTemp;var weatherCloud;var weatherAPI=new apiCall();weatherAPI.get(apiURL,function(weatherData){weatherData=JSON.parse(weatherData);var cityData=weatherData.city;weatherData=weatherData.list;loadingIcon.classList.add('hidden');weatherContentArea.innerHTML+=`
+            <a class="pure-u-24-24 weatherMain" rel="amphtml">
+                <h2 class="newsItem-title">${cityData.name}, ${cityData.country}</h2>
+                <h4 class="newsItem-title">Last updated: ${currentTime.toLocaleTimeString()}</h4>                
+            </a>`;for(var day in weatherData){weatherDesc=weatherData[day].weather[0].main;weatherTemp=Math.round(weatherData[day].temp.day);weatherCloud=weatherData[day].clouds;weatherDate=weatherData[day].dt;weatherDate=convertToDay(weatherDate);console.dir(weatherData[day].weather);weatherContentArea.innerHTML+=`
+            <a class="pure-u-24-24 weatherItem" rel="amphtml">
+                <h2 class="pure-u-12-24 weatherItem-date">${weatherDate}</h2>            
+                <h2 class="pure-u-11-24 weatherItem-desc">${weatherDesc}</h2>
+                <h2 class="pure-u-12-24 weatherItem-temp">${weatherTemp}°C</h2>
+                <h2 class="pure-u-11-24 weatherItem-cloud">${weatherCloud}% Cloud</h2>
+            </a>`}})};var apiCall=function(){this.get=function(apiURL,apiCallbackFunc){var apiRequest=new XMLHttpRequest();apiRequest.onreadystatechange=function(){if(apiRequest.readyState==4&&apiRequest.status==200){apiCallbackFunc(apiRequest.responseText);refreshIcon.classList.remove('refresh__rotate')}};apiRequest.open("GET",apiURL,!0);apiRequest.send(null)}};var refreshPage=function(){refreshIcon.classList.add('refresh__rotate');if(!newsContentArea.classList.contains('hidden')){newsContentArea.innerHTML="";loadNewsData()}
+else if(!weatherContentArea.classList.contains('hidden')){weatherContentArea.innerHTML="";loadWeatherData()}};var returnToMenu=function(){if(!articleContentArea.classList.contains('hidden')){articleContentArea.classList.add('hidden');newsContentArea.classList.remove('hidden')}
+else{newsContentArea.classList.add('hidden');weatherContentArea.classList.add('hidden');backBtnContainer.classList.add('hidden');refreshBtnContainer.classList.add('hidden');menuButtons.classList.remove('hidden')}};var convertToDay=function(time){var day=new Date(time*1000).getDay();switch(day){case 1:day='Monday';break;case 2:day='Tuesday';break;case 3:day='Wednesday';break;case 4:day='Thursday';break;case 5:day='Friday';break;case 6:day='Saturday';break;case 0:day='Sunday';break}
+return day}
